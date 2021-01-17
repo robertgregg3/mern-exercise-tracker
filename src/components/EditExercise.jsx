cfebc381b684058b67884aa4a6d84cb4c696e082
Robert Gregg <hi@robertgregg.co.uk>
2021-01-17 21:27:21 +0000

    added Edit exercise component

--- src/components/EditExercise.jsx
+++ src/components/EditExercise.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
 class EditExercises extends Component {
   constructor(props) {
@@ -14,11 +17,24 @@ class EditExercises extends Component {
   }
 
   componentDidMount() {
+    axios
+      .get("http://localhost:5000/exercises/" + this.props.match.params.id)
+      .then((response) => {
+        this.setState({
+          username: response.data.username,
+          description: response.data.description,
+          duration: response.data.duration,
+          date: new Date(response.data.date),
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
     axios.get("http://localhost:5000/users/").then((response) => {
       if (response.data.length > 0) {
         this.setState({
           users: response.data.map((user) => user.username),
-          username: response.data[0].username,
         });
       }
     });
@@ -49,7 +65,10 @@ class EditExercises extends Component {
 
     console.log(exercise);
     axios
-      .post("http://localhost:5000/exercises/add", exercise)
+      .post(
+        "http://localhost:5000/exercises/update" + this.props.match.params.id,
+        exercise
+      )
       .then((res) => console.log(res.data));
 
     window.location = "/";
@@ -58,7 +77,7 @@ class EditExercises extends Component {
   render() {
     return (
       <div>
-        <h3>Create New Exercise</h3>
+        <h3>Edit Exercise</h3>
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Username: </label>
@@ -110,7 +129,7 @@ class EditExercises extends Component {
           <div className="form-group">
             <input
               type="submit"
-              value="Create exercise log"
+              value="Edit exercise log"
               className="btn btn-primary"
             />
           </div>
--- src/components/ExercisesList.jsx
+++ src/components/ExercisesList.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Exercise from "./Exercise";
+import { Link } from "react-router-dom";
+
+const Exercise = (props) => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.exercise._id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExercise(props.exercise._id);
+        }}>
+        delete
+      </a>
+    </td>
+  </tr>
+);
 
 class ExerciseList extends Component {
   constructor(props) {

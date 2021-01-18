9196980e6da28febea8d4c61e09c635789582044
Robert Gregg <hi@robertgregg.co.uk>
2021-01-18 09:32:27 +0000

    Added responsive nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,38 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
+  state = {
+    navabarToggled: true,
+  };
+
+  handleNav = () => {
+    if (!this.state.navabarToggled) {
+      this.setState({ navabarToggled: true });
+    } else {
+      this.setState({ navabarToggled: false });
+    }
+  };
+
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">
           ExcerTracker
         </Link>
-        <div className="collpase navbar-collapse">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" onClick={this.handleNav}></span>
+        </button>
+        <div
+          className={`collpase navbar-collapse ${
+            this.state.navabarToggled ? "active" : ""
+          } `}>
           <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
               <Link to="/" className="nav-link">
